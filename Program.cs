9873f68f635f7cc0f8e9c2510d631bb3bc646117
rennylson-lemos <renylsonlemos4321@gmail.com>
2021-12-11 14:05:24 -0300

    aula02

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _212054_atvtotaldias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double valor_cmp, percentutal;
+                Console.Write("imforme o valor da compra: ");
+                valor_cmp = double.Parse(Console.ReadLine());
+
+                Console.Write("imforme o percentual de lucro: ");
+                percentutal = double.Parse(Console.ReadLine());
+
+                valor_cmp += valor_cmp * (percentutal / 100);
+
+                Console.Write("o valor de venda sera:" + valor_cmp.ToString("c"));
+
+               //codico do rennylson
+            }
+
+            catch(FormatException)
+            {
+                Console.WriteLine("valor digitado invalido");
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine(erro.Message);
+           
+            
+            }
+            Console.ReadKey();
+        }    
+    }
+}
